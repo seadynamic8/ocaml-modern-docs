ee66523b84b01d1cfb715e619f0582caff6b51ad
seadynamic8 <seadynamic8@zoho.com>
2018-11-05 10:32:14 -0500

    Refactor

--- src/docs.bs.js
+++ src/docs.bs.js
@@ -3,9 +3,9 @@
 
 var Fs = require("fs");
 var List = require("bs-platform/lib/js/list.js");
-var Vdom = require("bucklescript-tea/src-ocaml/vdom.js");
 var Types = require("./types.bs.js");
 var $$String = require("bs-platform/lib/js/string.js");
+var Content = require("./content.bs.js");
 var Decoder = require("./decoder.bs.js");
 var License = require("./license.bs.js");
 var Sidebar = require("./sidebar.bs.js");
@@ -375,155 +375,6 @@ function update(model, msg) {
   }
 }
 
-function innerHTML(html) {
-  return Vdom.prop("innerHTML", html);
-}
-
-function view_element_header(name, category, content, element_html) {
-  return /* :: */[
-          Tea_html.h5(undefined, undefined, /* :: */[
-                Tea_html.id(name),
-                /* :: */[
-                  Tea_html.class$prime("element-item"),
-                  /* [] */0
-                ]
-              ], /* :: */[
-                Tea_html.span(undefined, undefined, /* :: */[
-                      Tea_html.class$prime("category"),
-                      /* [] */0
-                    ], /* :: */[
-                      Tea_html.text(category),
-                      /* [] */0
-                    ]),
-                /* :: */[
-                  Tea_html.text(name + content),
-                  /* [] */0
-                ]
-              ]),
-          element_html
-        ];
-}
-
-function view_element_info(info, element_html) {
-  if (info !== undefined) {
-    return /* :: */[
-            Tea_html.div(undefined, undefined, /* :: */[
-                  Tea_html.class$prime("info"),
-                  /* :: */[
-                    Vdom.prop("innerHTML", info),
-                    /* [] */0
-                  ]
-                ], /* [] */0),
-            element_html
-          ];
-  } else {
-    return element_html;
-  }
-}
-
-function view_element_type_table(type_table, element_html) {
-  if (type_table !== undefined) {
-    return /* :: */[
-            Tea_html.table(undefined, undefined, /* :: */[
-                  Vdom.prop("innerHTML", type_table),
-                  /* [] */0
-                ], /* [] */0),
-            element_html
-          ];
-  } else {
-    return element_html;
-  }
-}
-
-function parse_exception(exec_parameter) {
-  if (exec_parameter !== undefined) {
-    return " of " + exec_parameter;
-  } else {
-    return "";
-  }
-}
-
-function view_element(element) {
-  switch (element.tag | 0) {
-    case 0 : 
-        return view_element_header(element[0], "type ", " = " + element[1], view_element_info(element[2], /* [] */0));
-    case 1 : 
-        return view_element_header(element[0], "type ", " =", view_element_type_table(element[1], view_element_info(element[2], /* [] */0)));
-    case 2 : 
-        return view_element_header(element[0], "val ", " : " + element[1], view_element_info(element[2], /* [] */0));
-    case 3 : 
-        return view_element_header(element[0], "exception ", parse_exception(element[1]), view_element_info(element[2], /* [] */0));
-    case 4 : 
-        return view_element_header(element[0], "module ", ": sig .. end", view_element_info(element[1], /* [] */0));
-    case 5 : 
-        return view_element_header(element[0], "module type ", ": sig .. end", view_element_info(element[1], /* [] */0));
-    
-  }
-}
-
-function view_elements(elements) {
-  return List.map((function (e) {
-                return Tea_html.li(undefined, undefined, /* :: */[
-                            Tea_html.class$prime("element"),
-                            /* [] */0
-                          ], view_element(e));
-              }), elements);
-}
-
-function view_section_name(section_name) {
-  if (section_name !== undefined) {
-    return Tea_html.h2(undefined, undefined, /* :: */[
-                Tea_html.class$prime("subtitle"),
-                /* [] */0
-              ], /* :: */[
-                Tea_html.text(section_name),
-                /* [] */0
-              ]);
-  } else {
-    return Tea_html.span(undefined, undefined, /* [] */0, /* [] */0);
-  }
-}
-
-function view_section_info(section_info) {
-  if (section_info !== undefined) {
-    return Tea_html.div(undefined, undefined, /* :: */[
-                Tea_html.class$prime("info"),
-                /* :: */[
-                  Vdom.prop("innerHTML", section_info),
-                  /* [] */0
-                ]
-              ], /* [] */0);
-  } else {
-    return Tea_html.span(undefined, undefined, /* [] */0, /* [] */0);
-  }
-}
-
-function view_section(section) {
-  return Tea_html.li(undefined, undefined, /* :: */[
-              Tea_html.class$prime("section"),
-              /* [] */0
-            ], /* :: */[
-              view_section_name(section[/* section_name */0]),
-              /* :: */[
-                view_section_info(section[/* section_info */1]),
-                /* :: */[
-                  Tea_html.ul(undefined, undefined, /* [] */0, view_elements(section[/* elements */2])),
-                  /* :: */[
-                    Tea_html.ul(undefined, undefined, /* :: */[
-                          Tea_html.class$prime("sub-section"),
-                          /* [] */0
-                        ], List.map(view_section, section[/* sub_sections */3])),
-                    /* [] */0
-                  ]
-                ]
-              ]
-            ]);
-}
-
-function view_sections(module_item) {
-  return List.map(view_section, module_item[/* sections */2]);
-}
-
 function view_main(model) {
   var match = model[/* page */3];
   var name = match[/* name */0];
@@ -543,49 +394,9 @@ function view_main(model) {
               ];
     default:
       try {
-        var module_item = List.find((function (m) {
-                return m[/* module_name */0] === name;
-              }), model[/* module_list */1]);
-        return /* :: */[
-                Tea_html.main(undefined, undefined, /* :: */[
-                      Tea_html.id("module-content"),
-                      /* :: */[
-                        Tea_html.class$prime("content"),
-                        /* [] */0
-                      ]
-                    ], /* :: */[
-                      Tea_html.h1(undefined, undefined, /* :: */[
-                            Tea_html.class$prime("title"),
-                            /* [] */0
-                          ], /* :: */[
-                            Tea_html.text(module_item[/* module_name */0]),
-                            /* [] */0
-                          ]),
-                      /* :: */[
-                        Tea_html.div(undefined, undefined, /* :: */[
-                              Tea_html.class$prime("info"),
-                              /* :: */[
-                                Vdom.prop("innerHTML", module_item[/* module_info */1]),
-                                /* [] */0
-                              ]
-                            ], /* [] */0),
-                        /* :: */[
-                          Tea_html.hr(undefined, undefined, /* [] */0, /* [] */0),
-                          /* :: */[
-                            Tea_html.section(undefined, undefined, /* :: */[
-                                  Tea_html.id("elements"),
-                                  /* [] */0
-                                ], /* :: */[
-                                  Tea_html.ul(undefined, undefined, /* [] */0, view_sections(module_item)),
-                                  /* [] */0
-                                ]),
-                            /* [] */0
-                          ]
-                        ]
-                      ]
-                    ]),
-                /* [] */0
-              ];
+        return Content.view_content(List.find((function (m) {
+                          return m[/* module_name */0] === name;
+                        }), model[/* module_list */1]));
       }
       catch (exn){
         if (exn === Caml_builtin_exceptions.not_found) {
@@ -663,17 +474,6 @@ exports.sections_search_results = sections_search_results;
 exports.create_search_result = create_search_result;
 exports.get_search_results = get_search_results;
 exports.update = update;
-exports.innerHTML = innerHTML;
-exports.view_element_header = view_element_header;
-exports.view_element_info = view_element_info;
-exports.view_element_type_table = view_element_type_table;
-exports.parse_exception = parse_exception;
-exports.view_element = view_element;
-exports.view_elements = view_elements;
-exports.view_section_name = view_section_name;
-exports.view_section_info = view_section_info;
-exports.view_section = view_section;
-exports.view_sections = view_sections;
 exports.view_main = view_main;
 exports.view = view;
 exports.main = main;
--- src/docs.ml
+++ src/docs.ml
@@ -4,6 +4,7 @@ open Html
 open Types
 open Decoder
 open Sidebar
+open Content
 open Home_page
 open License
 
@@ -217,90 +218,6 @@ let update model msg =
 
 (* ------------ ViEW -------------- *)
 
-let innerHTML html =
-  Vdom.prop "innerHTML" html
-
-(* Elements *)
-
-let view_element_header name category content element_html =
-  h5
-    [ id name; class' "element-item" ]
-    [ span [ class' "category" ] [ text category ]
-    ; text (name ^ content )
-    ] :: element_html
-
-let view_element_info info element_html =
-  match info with
-  | Some info -> div [ class' "info"; innerHTML info ] [] :: element_html
-  | None -> element_html
-
-let view_element_type_table type_table element_html =
-  match type_table with
-  | Some type_table -> table [ innerHTML type_table ] [] :: element_html
-  | None -> element_html
-
-let parse_exception exec_parameter =
-  match exec_parameter with
-  | Some exec_parameter -> " of " ^ exec_parameter
-  | None -> ""
-
-let view_element element =
-  match element with
-  | Type (name, type_type, info) ->
-      []
-      |> view_element_info info
-      |> view_element_header name "type " (" = " ^ type_type)
-  | Typevariant (name, type_table, info) ->
-      []
-      |> view_element_info info
-      |> view_element_type_table type_table
-      |> view_element_header name "type " (" =")
-  | Function (name, func_annotation, info) ->
-      []
-      |> view_element_info info
-      |> view_element_header name "val " (" : " ^ func_annotation)
-  | Exception (name, exec_parameter, info) ->
-      []
-      |> view_element_info info
-      |> view_element_header name "exception " (parse_exception exec_parameter)
-  | Module (name, info) ->
-      []
-      |> view_element_info info
-      |> view_element_header name "module " (": sig .. end")
-  | Moduletype (name, info) ->
-      []
-      |> view_element_info info
-      |> view_element_header name "module type " (": sig .. end")
-
-let view_elements elements =
-  List.map (fun e -> li [ class' "element" ] (view_element e)) elements
-
-(* Sections *)
-
-let view_section_name section_name =
-  match section_name with
-  | Some name -> h2 [ class' "subtitle" ] [ text name ]
-  | None -> span [] []
-
-let view_section_info section_info =
-  match section_info with
-  | Some info -> div [ class' "info"; innerHTML info ] []
-  | None -> span [] []
-
-let rec view_section section =
-  li [ class' "section" ]
-    [ view_section_name section.section_name
-    ; view_section_info section.section_info
-    ; ul
-        []
-        (view_elements section.elements)
-    ; ul
-        [ class' "sub-section" ]
-        (List.map (fun ss -> view_section ss) section.sub_sections)
-    ]
-
-let view_sections module_item =
-  List.map (fun s -> view_section s) module_item.sections
 
 (* Main Content Page *)
 
@@ -313,20 +230,9 @@ let view_main model =
     ]
   | { name } ->
     try
-      let module_item =
-          model.module_list
-          |> List.find (fun m -> m.module_name = name)
-      in
-        [ main
-            [ id "module-content"; class' "content" ]
-            [ h1 [ class' "title" ] [ text module_item.module_name ]
-            ; div [ class' "info"; innerHTML module_item.module_info ] []
-            ; hr [] []
-            ; section
-              [ id "elements" ]
-              [ ul [] (view_sections module_item) ]
-            ]
-        ]
+      model.module_list
+      |> List.find (fun m -> m.module_name = name)
+      |> view_content
     with
       | Not_found ->
         [ main
